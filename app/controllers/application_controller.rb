bcbdc7ab538c057af8717135693f44a0fe7eeaeb
Jessica Simon <jsimon2789@gmail.com>
2016-01-12 22:16:42 -0500

    Remove Authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  USERS = { "districtsports" => Rails.application.secrets.login_password }
+  # USERS = { "districtsports" => Rails.application.secrets.login_password }
 
-  before_filter :authenticate
+  # before_filter :authenticate
 
-  def authenticate
-    if Rails.env.production?
-      authenticate_or_request_with_http_digest("Application") do |name|
-        USERS[name]
-      end
-    end
-  end
+  # def authenticate
+    # if Rails.env.production?
+      # authenticate_or_request_with_http_digest("Application") do |name|
+        # USERS[name]
+      # end
+    # end
+  # end
 
   helper_method :current_user
 
